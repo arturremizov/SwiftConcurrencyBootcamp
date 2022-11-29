2d1b7b7ffaa6c0887ec5c01f0cc10e312d52ab57
Artur <arturremizov@gmail.com>
2022-11-29 16:39:01 +0300

    perform concurrent methods with Async Let

---
+++ SwiftConcurrencyBootcamp/AsyncLetBootcamp.swift
@@ -0,0 +1,87 @@
+//
+//  AsyncLetBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Artur Remizov on 29.11.22.
+//
+
+import SwiftUI
+
+struct AsyncLetBootcamp: View {
+    
+    @State private var images: [UIImage] = []
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let url = URL(string: "https://picsum.photos/300")!
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
+                }
+            }
+            .navigationTitle("Async Let 🥳")
+            .onAppear {
+                Task {
+                    do {
+                        async let fetchTitle1 = fetchTitle()
+                        async let fetchImage1 = fetchImage()
+                       
+                        let (title, image) =  await (fetchTitle1, try fetchImage1)
+                        
+//                        async let fetchImage2 = fetchImage()
+//                        async let fetchImage3 = fetchImage()
+//                        async let fetchImage4 = fetchImage()
+//
+//                        let (image1, image2, image3, image4) = try await (fetchImage1, fetchImage2, fetchImage3, fetchImage4)
+//                        self.images.append(contentsOf: [image1, image2, image3, image4])
+                        
+//                        let image1 = try await fetchImage()
+//                        self.images.append(image1)
+//
+//                        let image2 = try await fetchImage()
+//                        self.images.append(image2)
+//
+//                        let image3 = try await fetchImage()
+//                        self.images.append(image3)
+//
+//                        let image4 = try await fetchImage()
+//                        self.images.append(image4)
+                        
+                    } catch {
+                        
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    func fetchTitle() async -> String {
+        return "NEW TITLE"
+    }
+    
+    func fetchImage() async throws -> UIImage {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
+    }
+}
+
+struct AsyncLetBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncLetBootcamp()
+    }
+}
