7f1ddd68fef1abc8eb62ceadd3520c743ebfb083
Artur <arturremizov@gmail.com>
2022-12-05 16:57:19 +0300

    Actors and non-isolated

---
+++ SwiftConcurrencyBootcamp/ActorsBootcamp.swift
@@ -0,0 +1,140 @@
+//
+//  ActorsBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Artur Remizov on 5.12.22.
+//
+
+import SwiftUI
+
+class MyDataManager {
+    
+    static let instance = MyDataManager()
+    private init() { }
+    
+    var data: [String] = []
+    private let queue = DispatchQueue(label: "com.MyApp.MyDataManager")
+    
+    func getRandomData(completionHandler: @escaping (_ title: String?) -> Void) {
+        queue.async {
+            self.data.append(UUID().uuidString)
+            print(Thread.current)
+            completionHandler(self.data.randomElement())
+        }
+    }
+}
+
+actor MyActorDataManager {
+    
+    static let instance = MyActorDataManager()
+    private init() { }
+    
+    var data: [String] = []
+    
+    nonisolated let myRandomText = "asdasdsadsadasd"
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        print(Thread.current)
+        return self.data.randomElement()
+    }
+    
+    nonisolated func getSavedData() -> String {
+        return "NEW DATA"
+    }
+}
+
+struct HomeView: View {
+    
+    let manager = MyActorDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onAppear {
+            let string = manager.getSavedData
+            let randomText = manager.myRandomText
+        }
+        .onReceive(timer) { _ in
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run(body: {
+                        self.text = data
+                    })
+                }
+            }
+            
+//            DispatchQueue.global(qos: .background).async {
+//                manager.getRandomData { title in
+//                    if let data = title {
+//                        DispatchQueue.main.async {
+//                            self.text = data
+//                        }
+//                    }
+//                }
+//            }
+        }
+    }
+}
+
+struct BrowseView: View {
+    
+    let manager = MyActorDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run(body: {
+                        self.text = data
+                    })
+                }
+            }
+//            DispatchQueue.global(qos: .background).async {
+//                manager.getRandomData { title in
+//                    if let data = title {
+//                        DispatchQueue.main.async {
+//                            self.text = data
+//                        }
+//                    }
+//                }
+//            }
+        }
+    }
+}
+
+
+struct ActorsBootcamp: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            BrowseView()
+                .tabItem {
+                    Label("Browse", systemImage: "magnifyingglass")
+                }
+        }
+    }
+}
+
+struct ActorsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ActorsBootcamp()
+    }
+}
